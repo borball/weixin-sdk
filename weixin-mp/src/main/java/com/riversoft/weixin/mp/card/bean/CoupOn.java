1a6f37c75754ae08f0511a9856517ee090e724c6
borball <borball.zhai@ericsson.com>
2015-11-29 11:14:00 -0500

    卡券

--- weixin-mp/src/main/java/com/riversoft/weixin/mp/card/bean/CoupOn.java
+++ weixin-mp/src/main/java/com/riversoft/weixin/mp/card/bean/CoupOn.java
@@ -3,5 +3,5 @@ package com.riversoft.weixin.mp.card.bean;
 /**
  * Created by exizhai on 11/28/2015.
  */
-public class CoupOn {
+public class Coupon {
 }
--- weixin-mp/src/main/java/com/riversoft/weixin/mp/card/bean/GroupOn.java
+++ weixin-mp/src/main/java/com/riversoft/weixin/mp/card/bean/GroupOn.java
@@ -3,5 +3,5 @@ package com.riversoft.weixin.mp.card.bean;
 /**
  * Created by exizhai on 11/28/2015.
  */
-public class GroupOn {
+public class Groupon {
 }
