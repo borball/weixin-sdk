09d9c9dc56be61d3927c450a6e05c2ebb4fcb123
borball <borball.zhai@ericsson.com>
2016-02-15 16:59:53 -0500

    [企业号] fixed get users by tag id issue

--- weixin-qydev/src/main/java/com/riversoft/weixin/qy/contact/Tags.java
+++ weixin-qydev/src/main/java/com/riversoft/weixin/qy/contact/Tags.java
@@ -91,7 +91,7 @@ public class Tags {
 
     public TagUsers getUsers(int id) {
         String url = WxEndpoint.get("url.tag.user.get");
-        logger.debug("get tag: {}", id);
+        logger.debug("get tag user: {}", id);
         String response = wxClient.get(String.format(url, id));
         TagUsers tagUsers = JsonMapper.defaultMapper().fromJson(response, TagUsers.class);
         tagUsers.setTagId(id);
