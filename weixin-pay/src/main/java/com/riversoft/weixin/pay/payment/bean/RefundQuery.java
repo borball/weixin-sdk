ae300c51239bef70addefaf2fb2c97ccccd96194
borball <borball.zhai@ericsson.com>
2016-12-29 22:39:48 -0500

    fixed issue #28

--- weixin-pay/src/main/java/com/riversoft/weixin/pay/payment/bean/RefundQuery.java
+++ weixin-pay/src/main/java/com/riversoft/weixin/pay/payment/bean/RefundQuery.java
@@ -1,7 +1,13 @@
 package com.riversoft.weixin.pay.payment.bean;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.riversoft.weixin.pay.base.BaseResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @borball on 5/17/2016.
@@ -29,7 +35,15 @@ public class RefundQuery extends BaseResponse {
     @JsonProperty("refund_count")
     private int refundCount;
 
-    //TODO others
+    @JsonProperty("refund_count")
+    private String refundAccount;
+
+    protected Map<String,Object> others = new HashMap<>();
+
+    @JsonAnySetter
+    public void set(String name, Object value) {
+        others.put(name, value);
+    }
 
     public String getTransactionId() {
         return transactionId;
@@ -85,5 +99,197 @@ public class RefundQuery extends BaseResponse {
 
     public void setRefundCount(int refundCount) {
         this.refundCount = refundCount;
+    }
+
+    public String getRefundAccount() {
+        return refundAccount;
+    }
+
+    public void setRefundAccount(String refundAccount) {
+        this.refundAccount = refundAccount;
+    }
+
+    public List<Refund> getRefunds(){
+        List<Refund> refunds = new ArrayList<>();
+        if(others.size() > 0) {
+            for (int i = 0; i < refundCount; i++) {
+                Refund refund = new Refund();
+
+                if(others.containsKey("out_refund_no_" + i)) {
+                    refund.setOutRefundNo((String)others.get("out_refund_no_" + i));
+                }
+                if(others.containsKey("refund_id_" + i)) {
+                    refund.setRefundId((String)others.get("refund_id_" + i));
+                }
+                if(others.containsKey("refund_channel_" + i)) {
+                    refund.setRefundChannel((String)others.get("refund_channel_" + i));
+                }
+                if(others.containsKey("refund_fee_" + i)) {
+                    refund.setRefundFee(Integer.valueOf(others.get("refund_fee_" + i).toString()));
+                }
+                if(others.containsKey("settlement_refund_fee_" + i)) {
+                    refund.setSettlementRefundFee(Integer.valueOf(others.get("settlement_refund_fee_" + i).toString()));
+                }
+                if(others.containsKey("coupon_type_" + i)) {
+                    refund.setCouponType((String)others.get("coupon_type_" + i));
+                }
+                if(others.containsKey("coupon_refund_fee_" + i)) {
+                    refund.setCouponRefundFee(Integer.valueOf(others.get("coupon_refund_fee_" + i).toString()));
+                }
+                if(others.containsKey("coupon_refund_count_" + i)) {
+                    int count = Integer.valueOf(others.get("coupon_refund_count_" + i).toString());
+                    refund.setCouponRefundCount(count);
+                    List<CouponRefund> couponRefunds = new ArrayList();
+                    for (int j = 0; j < count; j++) {
+                        CouponRefund couponRefund = new CouponRefund();
+                        if(others.containsKey("coupon_refund_id_" + i + "_" + j)) {
+                            couponRefund.setCouponRefundId((String)others.get("coupon_refund_id_" + i + "_" + j));
+                        }
+                        if(others.containsKey("coupon_refund_fee_" + i + "_" + j)) {
+                            couponRefund.setCouponRefundFee(Integer.valueOf(others.get("coupon_refund_fee_" + i + "_" + j).toString()));
+                        }
+
+                        couponRefunds.add(couponRefund);
+                    }
+
+                    refund.setCouponRefunds(couponRefunds);
+                }
+                if(others.containsKey("refund_status_" + i)) {
+                    refund.setRefundStatus((String)others.get("refund_status_" + i));
+                }
+                if(others.containsKey("refund_recv_accout_" + i)) {
+                    refund.setRefundReceiveAccount((String)others.get("refund_recv_accout_" + i));
+                }
+
+                refunds.add(refund);
+            }
+        }
+        return refunds;
+    }
+
+    public class Refund {
+
+        private String outRefundNo;
+        private String refundId;
+        private String refundChannel;
+        private int refundFee;
+        private int settlementRefundFee;
+        private String couponType;
+        private int couponRefundFee;
+        private int couponRefundCount;
+        private String refundStatus;
+        private String refundReceiveAccount;
+
+        private List<CouponRefund> couponRefunds;
+
+        public String getOutRefundNo() {
+            return outRefundNo;
+        }
+
+        public void setOutRefundNo(String outRefundNo) {
+            this.outRefundNo = outRefundNo;
+        }
+
+        public String getRefundId() {
+            return refundId;
+        }
+
+        public void setRefundId(String refundId) {
+            this.refundId = refundId;
+        }
+
+        public String getRefundChannel() {
+            return refundChannel;
+        }
+
+        public void setRefundChannel(String refundChannel) {
+            this.refundChannel = refundChannel;
+        }
+
+        public int getRefundFee() {
+            return refundFee;
+        }
+
+        public void setRefundFee(int refundFee) {
+            this.refundFee = refundFee;
+        }
+
+        public int getSettlementRefundFee() {
+            return settlementRefundFee;
+        }
+
+        public void setSettlementRefundFee(int settlementRefundFee) {
+            this.settlementRefundFee = settlementRefundFee;
+        }
+
+        public String getCouponType() {
+            return couponType;
+        }
+
+        public void setCouponType(String couponType) {
+            this.couponType = couponType;
+        }
+
+        public int getCouponRefundFee() {
+            return couponRefundFee;
+        }
+
+        public void setCouponRefundFee(int couponRefundFee) {
+            this.couponRefundFee = couponRefundFee;
+        }
+
+        public int getCouponRefundCount() {
+            return couponRefundCount;
+        }
+
+        public void setCouponRefundCount(int couponRefundCount) {
+            this.couponRefundCount = couponRefundCount;
+        }
+
+        public String getRefundStatus() {
+            return refundStatus;
+        }
+
+        public void setRefundStatus(String refundStatus) {
+            this.refundStatus = refundStatus;
+        }
+
+        public String getRefundReceiveAccount() {
+            return refundReceiveAccount;
+        }
+
+        public void setRefundReceiveAccount(String refundReceiveAccount) {
+            this.refundReceiveAccount = refundReceiveAccount;
+        }
+
+        public List<CouponRefund> getCouponRefunds() {
+            return couponRefunds;
+        }
+
+        public void setCouponRefunds(List<CouponRefund> couponRefunds) {
+            this.couponRefunds = couponRefunds;
+        }
+    }
+
+    public class CouponRefund {
+
+        private String couponRefundId;
+        private int couponRefundFee;
+
+        public String getCouponRefundId() {
+            return couponRefundId;
+        }
+
+        public void setCouponRefundId(String couponRefundId) {
+            this.couponRefundId = couponRefundId;
+        }
+
+        public int getCouponRefundFee() {
+            return couponRefundFee;
+        }
+
+        public void setCouponRefundFee(int couponRefundFee) {
+            this.couponRefundFee = couponRefundFee;
+        }
     }
 }
---
+++ weixin-pay/src/test/java/com/riversoft/weixin/pay/payment/RefundQueryTest.java
@@ -0,0 +1,66 @@
+package com.riversoft.weixin.pay.payment;
+
+import com.riversoft.weixin.common.util.XmlObjectMapper;
+import com.riversoft.weixin.pay.payment.bean.RefundQuery;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @borball on 12/29/2016.
+ */
+public class RefundQueryTest {
+
+    @Test
+    public void testXml2Bean() throws IOException {
+        RefundQuery refundQuery = XmlObjectMapper.defaultMapper().fromXml(getXml(), RefundQuery.class);
+        Assert.assertNotNull(refundQuery);
+        Assert.assertNotNull(refundQuery.getRefunds());
+        Assert.assertEquals(2, refundQuery.getRefunds().size());
+
+    }
+
+    private String getXml(){
+        return "<?xml version=\"1.0\"?>\n" +
+                "<xml>\n" +
+                "   <appid><![CDATA[wx2421b1c4370ec43b]]></appid>\n" +
+                "   <mch_id><![CDATA[10000100]]></mch_id>\n" +
+                "   <nonce_str><![CDATA[TeqClE3i0mvn3DrK]]></nonce_str>\n" +
+                "   <out_refund_no_0><![CDATA[1415701182]]></out_refund_no_0>\n" +
+                "   <out_trade_no><![CDATA[1415757673]]></out_trade_no>\n" +
+                "   <refund_count>2</refund_count>\n" +
+                "   <refund_fee_0>10</refund_fee_0>\n" +
+                "   <refund_id_0><![CDATA[2008450740201411110000174436]]></refund_id_0>\n" +
+                "   <refund_status_0><![CDATA[PROCESSING]]></refund_status_0>\n" +
+                "   <refund_channel_0><![CDATA[ORIGINAL]]></refund_channel_0>\n" +
+                "   <settlement_refund_fee_0>10</settlement_refund_fee_0>\n" +
+                "   <coupon_type_0><![CDATA[CASH]]></coupon_type_0>\n" +
+                "   <coupon_refund_fee_0>5</coupon_refund_fee_0>\n" +
+                "   <coupon_refund_count_0>2</coupon_refund_count_0>\n" +
+                "   <coupon_refund_id_0_0><![CDATA[coupon_0_0]]></coupon_refund_id_0_0>\n" +
+                "   <coupon_refund_id_0_1><![CDATA[coupon_0_1]]></coupon_refund_id_0_1>\n" +
+                "   <coupon_refund_id_1_0><![CDATA[coupon_1_0]]></coupon_refund_id_1_0>\n" +
+                "   <coupon_refund_id_1_1><![CDATA[coupon_1_1]]></coupon_refund_id_1_1>\n" +
+                "   <coupon_refund_fee_0_0>100</coupon_refund_fee_0_0>\n" +
+                "   <coupon_refund_fee_0_1>101</coupon_refund_fee_0_1>\n" +
+                "   <coupon_refund_fee_1_0>110</coupon_refund_fee_1_0>\n" +
+                "   <coupon_refund_fee_1_1>111</coupon_refund_fee_1_1>    \n" +
+                "   <refund_recv_accout_0><![CDATA[receive account 0]]></refund_recv_accout_0>\n" +
+                "   <refund_fee_1>10</refund_fee_1>\n" +
+                "   <refund_id_1><![CDATA[2008450740201411110000174436]]></refund_id_1>\n" +
+                "   <refund_status_1><![CDATA[PROCESSING]]></refund_status_1>\n" +
+                "   <refund_channel_1><![CDATA[ORIGINAL]]></refund_channel_1>\n" +
+                "   <settlement_refund_fee_1>10</settlement_refund_fee_1>\n" +
+                "   <coupon_type_1><![CDATA[CASH]]></coupon_type_1>\n" +
+                "   <coupon_refund_fee_1>5</coupon_refund_fee_1>\n" +
+                "   <coupon_refund_count_1>2</coupon_refund_count_1>\n" +
+                "   <refund_recv_accout_1><![CDATA[receive account 0]]></refund_recv_accout_1>   \n" +
+                "   <result_code><![CDATA[SUCCESS]]></result_code>\n" +
+                "   <return_code><![CDATA[SUCCESS]]></return_code>\n" +
+                "   <return_msg><![CDATA[OK]]></return_msg>\n" +
+                "   <sign><![CDATA[1F2841558E233C33ABA71A961D27561C]]></sign>\n" +
+                "   <transaction_id><![CDATA[1008450740201411110005820873]]></transaction_id>\n" +
+                "</xml>\n";
+    }
+}
