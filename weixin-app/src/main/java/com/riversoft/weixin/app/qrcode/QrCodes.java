2dca1de8e0c635e7d6ba7a8f3c8243a367ff88f9
borball <borball.zhai@ericsson.com>
2017-01-16 10:57:29 -0500

    fixed issue #31

--- weixin-app/src/main/java/com/riversoft/weixin/app/qrcode/QrCodes.java
+++ weixin-app/src/main/java/com/riversoft/weixin/app/qrcode/QrCodes.java
@@ -2,9 +2,12 @@ package com.riversoft.weixin.app.qrcode;
 
 import com.riversoft.weixin.app.AppWxClientFactory;
 import com.riversoft.weixin.app.base.AppSetting;
+import com.riversoft.weixin.app.base.WxEndpoint;
 import com.riversoft.weixin.common.WxClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
 
 /**
  * 二维码工具
@@ -30,4 +33,24 @@ public class QrCodes {
         this.wxClient = wxClient;
     }
 
+    /**
+     * 获取小程序页面二维码
+     * @param path, path 需要在 app.json 的 pages 中定义
+     * @return
+     */
+    public InputStream create(String path) {
+        return create(path, 430);
+    }
+
+    /**
+     * 获取小程序页面二维码
+     * @param path, path 需要在 app.json 的 pages 中定义
+     * @param size
+     * @return
+     */
+    public InputStream create(String path, int size) {
+        String url = WxEndpoint.get("url.qrcode.create");
+        String json = "{\"path\": \"%s\", \"width\": %s}";
+        return wxClient.copyStream(url, String.format(json, path, size));
+    }
 }
